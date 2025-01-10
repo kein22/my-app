d7c7990870c5c7563a4fc23a61a63456c96d522e
kein22 <148049256+kein22@users.noreply.github.com>
2025-01-10 12:14:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; // Import React
+
+// Định nghĩa hàm App
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App; // Xuất App để sử dụng trong index.js
